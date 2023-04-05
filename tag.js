7ded70e1dcae5a97de80cf9277ad4e4a331f902e
zheming.ge <zheming.ge@ekunde.com>
2023-04-05 23:42:14 +0800

    🐞 fix(tag): fix bug

--- tag.js
+++ tag.js
@@ -49,73 +49,10 @@ async function hasNewCommitsSinceLastTag(lastTag) {
   return parseInt(newCommits, 10) > 0;
 }
 
-// async function main() {
-//   try {
-//     const latestTag = await executeCommand('git describe --tags --abbrev=0').catch(() => '1.0.0');
-//     const [major, minor, patch] = latestTag.split('.').map(Number);
-
-//     readline.question('请选择更新类型 (1: 大更新, 2: 小更新, 3: 修复bug): ', (updateType) => {
-//       let newTag;
-//       switch (updateType) {
-//       case '1':
-//         newTag = `${major + 1}.0.0`;
-//         break;
-//       case '2':
-//         newTag = `${major}.${minor + 1}.0`;
-//         break;
-//       case '3':
-//         newTag = `${major}.${minor}.${patch + 1}`;
-//         break;
-//       default:
-//         console.log('输入错误，请选择正确的更新类型。');
-//         readline.close();
-//         process.exit(1);
-//       }
-
-//       console.log(`新标签: ${newTag}`);
-
-//       executeCommand(`git diff ${latestTag} HEAD`)
-//         .then((diff) => {
-//           console.log('变更详情：');
-//           console.log(diff);
-
-//           executeCommand(`git tag -a ${newTag} -m "Release ${newTag}"`)
-//             .then(() => {
-//               console.log(`已创建新标签：${newTag}`);
-//               const zipName = `project-${newTag}.zip`;
-//               const tarName = `project-${newTag}.tar.gz`;
-
-//               Promise.all([
-//                 executeCommand(`git archive --format=zip --output=${zipName} ${newTag}`),
-//                 executeCommand(`git archive --format=tar.gz --output=${tarName} ${newTag}`),
-//               ])
-//                 .then(() => {
-//                   console.log(`已生成压缩包：${zipName} 和 ${tarName}`);
-//                   readline.close();
-//                 })
-//                 .catch((error) => {
-//                   console.error(`生成压缩包时出错：${error}`);
-//                   readline.close();
-//                 });
-//             })
-//             .catch((error) => {
-//               console.error(`创建标签时出错：${error}`);
-//               readline.close();
-//             });
-//         })
-//         .catch((error) => {
-//           console.error(`获取变更详情时出错：${error}`);
-//           readline.close();
-//         });
-//     });
-//   } catch (error) {
-//     console.error(`执行时出错：${error}`);
-//   }
-// }
-
 async function main() {
   let isFirst = false;
   try {
+    let latestTag = await executeCommand('git describe --tags --abbrev=0').catch(() => null);
     if (await hasUncommittedChanges()) {
       console.error('There are uncommitted changes. Please commit or stash them before creating a new tag.');
       process.exit(1);
@@ -123,7 +60,6 @@ async function main() {
 
     if (await hasUnpushedCommits()) {
       console.error('There are unpushed commits. Please push them before creating a new tag.');
-      process.exit(1);
       process.exit(1);
     }
 
@@ -134,8 +70,6 @@ async function main() {
     console.log('Building the project...');
     await executeCommand('ng build ng-watermark --configuration production');
     console.log('Project build completed.');
-
-    let latestTag = await executeCommand('git describe --tags --abbrev=0').catch(() => null);
 
     if (!latestTag) {
       isFirst = true;
